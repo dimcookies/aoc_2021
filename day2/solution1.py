9b1c73b5f56a64ac9ff6f599f4772a4cc1f736ae
koukis <koukis@upstreamsystems.com>
2021-12-02 08:57:07 +0200

    Days 1 & 2

---
+++ day2/solution1.py
@@ -0,0 +1,17 @@
+import my_input
+from utils import pr
+i_s = False
+i_puzzle = 1
+file_input = "sample" + str(i_puzzle) if i_s else "input" + str(i_puzzle) 
+
+input = my_input.readlines_array(file_input)
+pr(i_s, input)
+#print(len(list(filter(lambda x: input[x] < input[x+1] ,range(len(input) - 1)))))
+
+x = sum([int(x[1]) for x in input if x[0] == 'forward'])
+
+y = sum([int(x[1]) if x[0] == 'down' else (-1 * int(x[1])) for x in input if x[0] != 'forward'])
+
+print(x*y)
+
+        
---
+++ day2/solution2.py
@@ -0,0 +1,21 @@
+import my_input
+from utils import pr
+i_s = False
+i_puzzle = 1
+file_input = "sample" + str(i_puzzle) if i_s else "input" + str(i_puzzle) 
+
+input = my_input.readlines_array(file_input)
+pr(i_s, input)
+state = [0,0,0]
+for i in input:
+    pr(i_s, state)
+    if i[0] == 'down':
+        state[2]+=int(i[1])
+    elif i[0] == 'up':
+        state[2]-=int(i[1])
+    else:
+        state[0] = state[0]+ int(i[1])
+        state[1] = state[1]+ int(i[1]) * state[2]
+
+print(state, state[0] * state[1])        
+        
